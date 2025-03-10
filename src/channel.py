96636751fdd104f3b97284bca4c5dc76d05459f1
Kristina Baraboshkina <kris.baraboshkina@gmail.com>
2025-03-11 03:10:21 +0800

    Class Channel created

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,37 @@
+import json
+import os
 
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
+
+import isodate
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+    api_key: str = os.getenv('YT_API_KEY')
+
+    if api_key is None:
+        raise ValueError("Ошибка: API-ключ не найден в переменных окружения!")
+
+    # создать специальный объект для работы с API
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id  # Сохраняем ID канала
+        self.channel_data = self.get_channel_info()  # Загружаем данные о канале
+
+    def get_channel_info(self):
+        """Получает данные о канале с помощью YouTube API"""
+        response = self.youtube.channels().list(
+            id=self.channel_id,
+            part='snippet,statistics'
+        ).execute()
+
+        return response['items'][0] if response['items'] else {}
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.channel_data, indent=2, ensure_ascii=False))
